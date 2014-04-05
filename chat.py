5fc9f9287e3417934debf21005739da47bab0b57
Jake Schmitz <jake@schmitzma.de>
2014-04-04 21:31:36 -0700

    starting point

--- chat.py
+++ chat.py
@@ -2,7 +2,7 @@ from gevent import monkey
 from flask import Flask, Response, render_template, request
 from socketio import socketio_manage
 from socketio.namespace import BaseNamespace
-
+from socketio.mixins import BroadcastMixin
 
 monkey.patch_all()
 
@@ -11,29 +11,34 @@ app.debug = True
 app.config['PORT'] = 5000
 
 
-class ChatNamespace(BaseNamespace):
-    def __init__(self):
+class ChatNamespace(BaseNamespace, BroadcastMixin):
+    def initialize(self):
         self.logger = app.logger
-        self.log = 'socketio session has started'
+        self.log("Socketio session started")
 
     def log(self, message):
-        self.logger.info('[{0}] {1}'.format(self.socket.sessid, message))
+        self.logger.info("[{0}] {1}".format(self.socket.sessid, message))
 
-    def rec_connect(self):
-        self.log("new connection")
+    def recv_connect(self):
+        self.log("New connection")
 
-    def rec_dissconnect(self):
-        self.log("Client dissconected")
+    def recv_disconnect(self):
+        self.log("Client disconnected")
 
-    def on_join(self, name):
-        self.log('%s joined chat' % name)
-        return True, name
+    def on_join(self, email):
+        self.log("%s joined chat" % email)
+        self.session['email'] = email
+        return True, email
 
     def on_message(self, message):
         self.log('got a message: %s' % message)
+        self.broadcast_event_not_me("message",{ "sender" : self.session["email"], "content" : message})
         return True, message
-@app.route("/", methods=['GET'])
-def index():
+
+
+
+@app.route('/', methods=['GET'])
+def landing():
     return render_template('index.html')
 
 @app.route('/socket.io/<path:remaining>')
@@ -41,7 +46,6 @@ def socketio(remaining):
     try:
         socketio_manage(request.environ, {'/chat': ChatNamespace}, request)
     except:
-        app.logger.error('Exception while handling socketio connection', exc_info=True)
-    return Response
-
-
+        app.logger.error("Exception while handling socketio connection",
+                         exc_info=True)
+    return Response()
