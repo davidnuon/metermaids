c42b1fbd5a43fba5258e842e807c62dd00b30eab
Jake Schmitz <jake@schmitzma.de>
2014-04-04 20:07:11 -0700

    basic chat server in need of templates

---
+++ chat.py
@@ -0,0 +1,40 @@
+from gevent import monkey
+from flask import Flask, Response, render_template, request
+from socketio import socketio_manage
+from socketio.namespace import BaseNamespace
+
+
+monkey.patch_all()
+
+app = Flask(__name__)
+app.debug = True
+app.config['PORT'] = 5000
+
+
+class ChatNamespace(BaseNamespace):
+    def __init__(self):
+        self.logger = app.logger
+        self.log = 'socketio session has started'
+
+    def log(self, message):
+        self.logger.info('[{0}] {1}'.format(self.socket.sessid, message))
+
+    def rec_connect(self):
+        self.log("new connection")
+
+    def rec_dissconnect(self):
+        self.log("Client dissconected")
+
+
+@app.route("/", methods=['GET'])
+def hello():
+    return "This is meter maids"
+
+
+@app.route('/socket.io/<path:remaining>')
+def socketio(remaining):
+    try:
+        socketio_manage(request.environ, {'/chat': ChatNamespace}, request)
+    except:
+        app.logger.error('Exception while handling socketio connection', exc_info=True)
+    return Response
